4cd334d1bfc87636ecc41c6d4f48f4e3b5f3d034
BryanCruz94 <93927873+BryanCruz94@users.noreply.github.com>
2021-12-01 08:30:41 -0500

    Subiendo commit 1 by BC

---
+++ EntradaSalidaDatos/src/Principal.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		/* Entrada y Salida de Datos por consola
+		Scanner entrada = new Scanner(System.in);
+		String texto;
+		
+		System.out.println("Digite una cadena: ");
+		texto = entrada.nextLine();
+		System.out.println("La cadena es: "+texto);
+		
+		char caracter;
+		System.out.println("Digite una letra: ");
+		caracter = entrada.next().charAt(3);
+		System.out.println("La cadena es: "+caracter);
+	*/
+		/* Entrada y salida por mensaje
+		String cadena;
+		char letra;
+		int entero;
+		double decimal;
+		
+		cadena = JOptionPane.showInputDialog("Digite una cadena");
+		entero =Integer.parseInt(JOptionPane.showInputDialog("Digite un entero"));
+		letra = JOptionPane.showInputDialog("Digite un caracter: ").charAt(0);
+		decimal = Double.parseDouble(JOptionPane.showInputDialog("Digite un decimal"));
+		System.out.println("La cadena introducida es: "+cadena);
+		JOptionPane.showInternalMessageDialog(null, "La cadena es "+cadena);
+		JOptionPane.showInternalMessageDialog(null, "La letra es "+letra);
+		JOptionPane.showInternalMessageDialog(null, "El entero es  "+entero);
+		JOptionPane.showInternalMessageDialog(null, "El numero decimal es "+decimal);
+		*/
+	}
+
+}
